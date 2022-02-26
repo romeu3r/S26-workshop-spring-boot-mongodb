de4f0d990e8e607b06b15c7344efdcf645eb104b
romeu3r <romeu3r@gmail.com>
2022-02-26 15:06:21 -0300

    Initial commit

---
+++ src/main/java/com/romeu3r/workshopcourse/WorkshopCourseApplication.java
@@ -0,0 +1,13 @@
+package com.romeu3r.workshopcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romeu3r/workshopcourse/WorkshopCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romeu3r.workshopcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
