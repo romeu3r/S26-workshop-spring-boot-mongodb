f753daf14e34d8cc6a82c4cd3b72e144e49a809c
romeu3r <romeu3r@gmail.com>
2022-02-26 20:00:34 -0300

    Obtendo um usuário por id

--- src/main/java/com/romeu3r/workshopcourse/services/UserService.java
+++ src/main/java/com/romeu3r/workshopcourse/services/UserService.java
@@ -2,10 +2,12 @@ package com.romeu3r.workshopcourse.services;
 
 import com.romeu3r.workshopcourse.domain.User;
 import com.romeu3r.workshopcourse.repository.UserRepository;
+import com.romeu3r.workshopcourse.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 }
---
+++ src/main/java/com/romeu3r/workshopcourse/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.romeu3r.workshopcourse.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
