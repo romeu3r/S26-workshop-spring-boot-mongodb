4e2f232b79f972aa29df3582a1e7f1904a40a12a
romeu3r <romeu3r@gmail.com>
2022-02-27 00:24:57 -0300

    Consulta simples com @Query

--- src/main/java/com/romeu3r/workshopcourse/repository/PostRepository.java
+++ src/main/java/com/romeu3r/workshopcourse/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.romeu3r.workshopcourse.repository;
 
 import com.romeu3r.workshopcourse.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 //    List<Post> findByTitleContaining(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
