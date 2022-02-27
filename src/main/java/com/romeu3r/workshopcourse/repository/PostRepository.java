c50740fe7ead2fd13b4ba1fffb501c2c31de0a74
romeu3r <romeu3r@gmail.com>
2022-02-27 00:17:03 -0300

    Consulta simples com query methods

--- src/main/java/com/romeu3r/workshopcourse/repository/PostRepository.java
+++ src/main/java/com/romeu3r/workshopcourse/repository/PostRepository.java
@@ -4,6 +4,10 @@ import com.romeu3r.workshopcourse.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+//    List<Post> findByTitleContaining(String text);
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
